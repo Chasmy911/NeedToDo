d322034c61358e3790989b32dee3b96f3c4eff34
chasmy911 <chasmy@inbox.ru>
2023-12-26 09:33:27 +0300

    add date and months

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { App_container, Calendar__header, Calendar__header__arrow__left, Calendar__header__arrow__right, Date_container } from './style';
+import { CalendarBox, Calendar__body, Calendar__days, Calendar__header, Calendar__pick__items__container, Calendar__unchoosable__year, Calendar__week__names, Date_container } from './style';
 import { useCalendar } from '../hooks/useCalendar';
-import { formatDate } from '../../utils/helpers/date/formatDate';
+import './styleDivs.css';
+import { checkIsToday } from '../../utils/helpers/date/checkIsToday';
+import { checkDateIsEqual } from '../../utils/helpers/date/checkDateIsEqual';
+import { ArrowBack, ArrowForward } from '@mui/icons-material'
 
 interface CalendarProps {
     locale?: string;
@@ -14,9 +17,8 @@ const Calendar: React.FC<CalendarProps> = ({
     locale = 'default',
     selectedDate: date,
     selectDate,
-    firstWeekDayNumber = 1
+    firstWeekDayNumber = 2
 }) => {
-
     const { functions, state } = useCalendar({
         locale,
         selectedDate: date,
@@ -24,36 +26,127 @@ const Calendar: React.FC<CalendarProps> = ({
     });
 
     return (
-        <div>
+        <CalendarBox >
+            <Calendar__header>
+                <ArrowBack onClick={() => functions.onClickArrow('left')}></ArrowBack>
 
-            <App_container>
-                <Calendar__header >
-
-                    <div>
-                        <Calendar__header__arrow__left /> {state.mode === 'days' && (
-                            <div>
-                                {state.monthesNames[state.selectedMonth.monthIndex].month} {state.selectedYear}
-                            </div>)}
-                        <Calendar__header__arrow__right />
-                        <div>  {state.mode === 'monthes' && (state.selectedYear)}</div>
-                        <div>
-                            {state.mode === 'years' && (
-                                <div>
-                                    {state.selectedYearsInterval[0]} -{' '}
-                                    {state.selectedYearsInterval[state.selectedYearsInterval.length - 1]}
-                                </div>
-                            )}
-                        </div>
-
-
+                {state.mode === 'days' && (
+                    <div aria-hidden onClick={() => functions.setMode('monthes')}>
+                        {state.monthesNames[state.selectedMonth.monthIndex].month} {state.selectedYear}
                     </div>
+                )}
+                {state.mode === 'monthes' && (
+                    <div aria-hidden onClick={() => functions.setMode('years')}>
+                        {state.selectedYear}
+                    </div>
+                )}
+                {state.mode === 'years' && (
+                    <div>
+                        {state.selectedYearsInterval[0]} -{' '}
+                        {state.selectedYearsInterval[state.selectedYearsInterval.length - 1]}
+                    </div>
+                )}
+                <ArrowForward onClick={() => functions.onClickArrow('right')}></ArrowForward>
 
+            </Calendar__header>
+            <Calendar__body>
+                {state.mode === 'days' && (
+                    <>
+                        <Calendar__week__names>
+                            {state.weekDaysNames.map((weekDaysName) => (
+                                <div key={weekDaysName.dayShort}>{weekDaysName.dayShort}</div>
+                            ))}
+                        </Calendar__week__names>
+                        <Calendar__days>
+                            {state.calendarDays.map((day) => {
+                                const isToday = checkIsToday(day.date);
+                                const isSelectedDay = checkDateIsEqual(day.date, state.selectedDay.date);
+                                const isAdditionalDay = day.monthIndex !== state.selectedMonth.monthIndex;
 
-                </Calendar__header>
-                calendar
-            </App_container>
-        </div>
+                                return (
+                                    <div
+                                        key={`${day.dayNumber}-${day.monthIndex}`}
+                                        aria-hidden
+                                        onClick={() => {
+                                            functions.setSelectedDay(day);
+                                            selectDate(day.date);
+                                        }}
+                                        className={[
+                                            'calendar__day',
+                                            isToday ? 'calendar__today__item' : '',
+                                            isSelectedDay ? 'calendar__selected__item' : '',
+                                            isAdditionalDay ? 'calendar__additional__day' : ''
+                                        ].join(' ')}
+                                    >
+                                        {day.dayNumber}
+                                    </div>
+                                );
+                            })}
+                        </Calendar__days>
+                    </>
+                )}
 
+                {state.mode === 'monthes' && (
+                    <Calendar__pick__items__container>
+                        {state.monthesNames.map((monthesName) => {
+                            const isCurrentMonth =
+                                new Date().getMonth() === monthesName.monthIndex &&
+                                state.selectedYear === new Date().getFullYear();
+                            const isSelectedMonth = monthesName.monthIndex === state.selectedMonth.monthIndex;
+
+                            return (
+                                <div
+                                    key={monthesName.month}
+                                    aria-hidden
+                                    onClick={() => {
+                                        functions.setSelectedMonthByIndex(monthesName.monthIndex);
+                                        functions.setMode('days');
+                                    }}
+                                    className={[
+                                        'calendar__pick__item',
+                                        isSelectedMonth ? 'calendar__selected__item' : '',
+                                        isCurrentMonth ? 'calendar__today__item' : ''
+                                    ].join(' ')}
+                                >
+                                    {monthesName.monthShort}
+                                </div>
+                            );
+                        })}
+                    </Calendar__pick__items__container>
+                )}
+
+                {state.mode === 'years' && (
+                    <Calendar__pick__items__container>
+                        <Calendar__unchoosable__year>{state.selectedYearsInterval[0] - 1}</Calendar__unchoosable__year>
+                        {state.selectedYearsInterval.map((year) => {
+                            const isCurrentYear = new Date().getFullYear() === year;
+                            const isSelectedYear = year === state.selectedYear;
+
+                            return (
+                                <div
+                                    key={year}
+                                    aria-hidden
+                                    onClick={() => {
+                                        functions.setSelectedYear(year);
+                                        functions.setMode('monthes');
+                                    }}
+                                    className={[
+                                        'calendar__pick__item',
+                                        isCurrentYear ? 'calendar__today__item' : '',
+                                        isSelectedYear ? 'calendar__selected__item' : ''
+                                    ].join(' ')}
+                                >
+                                    {year}
+                                </div>
+                            );
+                        })}
+                        <Calendar__unchoosable__year>
+                            {state.selectedYearsInterval[state.selectedYearsInterval.length - 1] + 1}
+                        </Calendar__unchoosable__year>
+                    </Calendar__pick__items__container>
+                )}
+            </Calendar__body>
+        </CalendarBox>
     );
 };
 
--- src/pages/Calendar/style.ts
+++ src/pages/Calendar/style.ts
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+
+export const CalendarBox = styled.div`
+ width: 800px;
+   border-radius: 5px;
+   text-transform: capitalize;
+   background-color: var(--background-color-calendar);
+   margin: 0 auto;`
+
 export const App_container = styled.div`
 	display: flex;
    justify-content: center;
@@ -8,7 +16,7 @@ export const App_container = styled.div`
    background-color: gray;
    height: 100vh;
    margin-inline: auto;
-   
+   gap: 20px;
 `;
 
 
@@ -17,7 +25,7 @@ export const Date_container = styled.div`
    justify-content: center;
    align-content: center;
    padding: 20px;
-  
+   gap: 20px;
    
 `;
 
@@ -36,15 +44,93 @@ export const Calendar__header = styled.div`
 
 
 
-export const Calendar__header__arrow__left = styled.div`  
-width: 5px;
-height: 10px;
-cursor: pointer;
-background-image: var(--images-arrow);`
+export const Calendar__body = styled.div` 
+border-radius: 0 0 5px 5px;
+padding: 5px;
+`
 
-export const Calendar__header__arrow__right = styled.div`
-   width: 5px;
-height: 10px;
-cursor: pointer;
-background-image: var(--images - arrow);
-transform: rotate(-180deg);` 
+
+export const Calendar__week__names = styled.div`
+  height: 20px;
+   font-weight: 600;
+   font-size: 8px;
+   color: var(--color-additional);
+   text-align: center;
+   align-items: center;
+   display: grid;
+   grid-template-columns: repeat(7, 1fr);
+   gap: 1px 1px;`
+
+export const Calendar__days = styled.div`
+  font-size: 10px;
+   font-weight: 400;
+   color: var(--color-text);
+   text-align: center;
+   display: grid;
+   text-align: center;
+   align-items: center;
+   grid-template-columns: repeat(7, 1fr);
+   grid-template-rows: 1fr;
+   gap: 10px 10px;`
+
+export const Calendar__day = styled.div`
+    border-radius: 5px;
+   padding: 5.5px 0px;
+   cursor: pointer;
+   
+&:hover {
+  background : grey;
+}`
+
+
+
+export const Calendar__additional__day = styled.div`
+   padding: 5.5px;
+   font-weight: 300;
+   cursor: pointer;
+   color: var(--color-placeholder);`
+
+
+export const Calendar__pick__item = styled.div`
+ padding: 9px 0px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   cursor: pointer;
+   border-radius: 5px;
+    
+&:hover {
+  background : grey;
+}
+   `
+
+
+export const Calendar__today__item = styled.div` 
+   background-color: var(--background - color - calendar - header);`
+
+
+export const Calendar__selected__item = styled.div`  
+color: var(--color - text);
+background-color: var(--color - additional);
+ 
+ &:hover {
+   background-color: var(--color - additional)}`
+
+
+
+export const Calendar__pick__items__container = styled.div`
+ font-weight: 400;
+   color: var(--color-text);
+   text-align: center;
+   display: grid;
+   grid-template-columns: repeat(3, 1fr);
+   grid-template-rows: repeat(4, 1fr);
+   gap: 1px 1px;
+   font-size: 12px;`
+
+export const Calendar__unchoosable__year = styled.div`
+   font-weight: 300;
+   padding: 9px 16px;
+   color: var(--color-placeholder);`
+
+
